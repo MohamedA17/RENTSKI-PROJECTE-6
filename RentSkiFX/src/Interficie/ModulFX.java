4baa0ab2c1a47a873b09f400d7fc50cfee6bfe12
paufb01 <77831219+paufb01@users.noreply.github.com>
2021-05-18 18:39:44 +0200

    Add files via upload

---
+++ RentSkiFX/src/Interficie/ModulFX.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Interficie;
+
+import java.awt.event.ActionEvent;
+import java.beans.EventHandler;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.text.Text;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class ModulFX extends Application {
+
+    BorderPane borderpane;
+    VBox menuVBOX, centreVBOX;
+    HBox horitzontalBox;
+    
+    
+    @Override
+    public void start(Stage primaryStage) {
+      
+    }
+    
+}
---
+++ RentSkiFX/src/rentskifx/RentSkiFX.java
@@ -0,0 +1,21 @@
+
+package rentskifx;
+
+//IMPORTS
+
+
+public class RentSkiFX {
+
+
+  
+    
+    
+    public static void main(String[] args) {
+        
+        
+       
+        
+    }
+
+    
+}
